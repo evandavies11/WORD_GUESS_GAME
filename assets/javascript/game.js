2d45a075f232828f67cd21866e6e45d633539f20
EVAN DAVIES <evandavies@EVANs-MacBook-Pro-2.local>
2019-06-20 14:27:15 -0500

    javanow

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var [
+    jedi 
+    ligtsaber
+    wookie
+    tatooine
+]
+
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+
+var remainingLetters = word.length; 
